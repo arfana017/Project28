8241b8b04c6d1f452c518283b1612c89f39b88d9
arfana017 <70667415+arfana017@users.noreply.github.com>
2020-12-04 00:21:59 -0800

    Update Launch.js

--- Launch.js
+++ Launch.js
@@ -1,17 +1,19 @@
 class Launcher{
-    constructor(bodyA, pointB){
-        var options = {
-            bodyA: bodyA,
-            pointB: pointB,
-            stiffness: 0.04,
-            length: 5
-        }
-
-
-        this.pointB = pointB
-        this.launcher = Constraint.create(options);
-        World.add(world, this.launcher);
-    }
+    constructor(body,anchor)
+	{
+		
+		var options={ bodyA:body,			 
+			pointB:anchor, 
+			stiffness:0.004, 
+			length:1
+			
+		}
+		
+		this.bodyA=body
+		this.pointB=anchor
+		this.launcher=Constraint.create(options)
+		World.add(world,this.launcher)
+	}
 
         attach(body){
             this.launcher.bodyA = body;
